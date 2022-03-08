efc058b5f1542e2c26feef91b9c5b5938a3a2a54
beutchowski <allopalto@icloud.com>
2022-03-08 16:03:52 +0100

    Tri par insertion ok

---
+++ TriParInsertion/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TriParInsertion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = new int[] { 1, 7, 3, 2, 0, 5, 0, 8 };
+
+            for(int cptPassage = 1; cptPassage < tab.Length; cptPassage++)
+            {
+                int temp = tab[cptPassage];
+                int cptElement = cptPassage;
+
+                while(cptElement - 1 >= 0 && temp < tab[cptElement-1])
+                {
+                    tab[cptElement] = tab[cptElement-1];
+                    cptElement --;
+                }
+                tab[cptElement] = temp;
+
+                foreach (int k in tab)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine("\n");
+
+            }
+
+        }
+    }
+}

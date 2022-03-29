993bd3e52951ac0d09821580ab4198f54e96f4bb
beutchowski <allopalto@icloud.com>
2022-03-29 15:39:12 +0200

    Temps de tri OK!

--- TempsDeTri/Program.cs
+++ TempsDeTri/Program.cs
@@ -6,7 +6,7 @@ namespace TempsDeTri
     internal class Program
     {
         static Random rand = new Random();
-        const int TAILLE = 50000;
+        const int TAILLE = 10000;
         static List<double> stockageSortieBubble;
         static List<double> stockageSortieInser;
         static List<double> stockageSortieSelec;
@@ -16,31 +16,36 @@ namespace TempsDeTri
             stockageSortieBubble = new List<double>();
 
 
-            for (int i = 1; i < 3; i++)
+            for (int i = 1; i < 4; i++)
             {
-                int[] tab = Tab(TAILLE);
-                TriParBubbleSort bubble = new TriParBubbleSort(tab);
-                TriParInsertion triInser = new TriParInsertion(tab);
-                TriParSelection triSelec = new TriParSelection(tab);
+                int[] tab1 = Tab(TAILLE);
+                int[] tab2 = Tab(TAILLE);
+                int[] tab3 = Tab(TAILLE);
+                TriParBubbleSort bubble = new TriParBubbleSort(tab1);
+                TriParInsertion triInser = new TriParInsertion(tab2);
+                TriParSelection triSelec = new TriParSelection(tab3);
+
 
                 bubble.Sort();
-                
 
                 triInser.Sort();
+
 
                 triSelec.Sort();
 
 
+                Console.WriteLine($"Tri BubbleSort test {i} : " + bubble.stopwatch.ElapsedMilliseconds + "ms");
+
+                Console.WriteLine($"Tri par insertion test {i} : " + triInser.stopwatch.ElapsedMilliseconds + "ms");
+
+                //Console.WriteLine($"Tri par insertion test {i} : {triInser.time:F5}ms");
+
+                Console.WriteLine($"Tri par sélection test {i} : " + triSelec.stopwatch.ElapsedMilliseconds + "ms");
+
             }
 
 
-            Console.WriteLine($"Tri BubbleSort test {i} : " + bubble.stopwatch.ElapsedMilliseconds + "ms");
 
-            Console.WriteLine($"Tri par insertion test {i} : " + triInser.stopwatch.ElapsedMilliseconds + "ms");
-
-            Console.WriteLine($"Tri par insertion test {i} : {triInser.time:F5}ms");
-
-            Console.WriteLine($"Tri par sélection test {i} : " + triSelec.stopwatch.ElapsedMilliseconds + "ms");
 
         }
 
@@ -49,10 +54,14 @@ namespace TempsDeTri
             int[] tab = new int[taille];
             for (int i = 0; i < tab.Length; i++)
             {
-                tab[i] = rand.Next(100000,1000000);
+                tab[i] = rand.Next(1,1000000);
             }
             return tab;
         }
-        
+
+
+
+
+
     }
 }

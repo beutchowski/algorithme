3327e74bd984f4a9177f3cf13a0b24361675f8b5
beutchowski <allopalto@icloud.com>
2022-03-29 16:40:46 +0200

    Fibonacci et calcul puissance OK!

---
+++ CalculPuissanceNbr/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CalculPuissanceNbr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(puissance(4, 2));
+        }
+
+        static int puissance(int mantisse, int exposant)
+        {
+            int nPow;
+            if (exposant > 0)
+            {
+                nPow = mantisse * puissance(mantisse, exposant - 1);
+            }
+            else
+                nPow = 1;
+            return nPow;
+        }
+    }
+}
+
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool onrecommence = true;
+            do
+            {
+                Console.Write("Le chiffre de Fibonacci désiré est le : ");
+                int fibo = int.Parse(Console.ReadLine());
+                double resultat = fibonacci(fibo);
+                Console.WriteLine($"\nRésultat : {resultat}");
+                Console.Write("ON recommence? ");
+                char ouinon = char.Parse(Console.ReadLine());
+                if (ouinon == 'o')
+                    onrecommence = true;
+                else if (ouinon == 'n')
+                    onrecommence = false;
+            }
+            while (onrecommence);
+
+
+            static int fibonacci(int n)
+            {
+                Console.Write($"| {n} |");
+                if ((n == 0) || (n == 1))
+                    return n;
+                else
+                    return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+        }
+    }
+}

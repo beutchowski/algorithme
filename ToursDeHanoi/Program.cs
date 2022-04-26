96ea27c52a407d3a12cd0f175c6d0ddfc13dfdfe
beutchowski <allopalto@icloud.com>
2022-04-26 16:25:51 +0200

    tour hanoi début

--- ToursDeHanoi/Program.cs
+++ ToursDeHanoi/Program.cs
@@ -4,9 +4,29 @@ namespace ToursDeHanoi
 {
     internal class Program
     {
+        int qtéPalet = 4;
+        int tourDépart = 1;
+        int tourInter = 2;
+        int tourFinale = 3;
+
+        int déplacements = 0;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
+
+        //Voir photo du pseudo code du prof
+
+        public void Déplace(int qtéPalet, int tourDépart, int tourFinale, int tourInter)
+        {
+
+            Déplace(qtéPalet - 1, tourDépart, tourFinale, tourInter);
+            Console.WriteLine();
+
+
+            return 0;
+        }
+
     }
 }

e73fad45ac7a9ef0422b89d58d324b7621849968
beutchowski <allopalto@icloud.com>
2022-04-26 15:48:15 +0200

    tour de hanoi

---
+++ CalculInterêtsComposés/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculInterêtsComposés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculIntéret(1000, 0.01, 4));
+        }
+
+        static decimal CalculIntéret(decimal somme, double intérêt, int nbrAnnées)
+        {
+            decimal produit;
+            if (nbrAnnées > 0)
+            {
+                produit = CalculIntéret(somme + somme * (decimal)intérêt, intérêt, nbrAnnées - 1);
+            }
+            else
+                produit = somme;
+            return produit;
+        }
+    }
+}
---
+++ OpérationFactorielle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OpérationFactorielle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool onrecommence = true;
+            do
+            {
+                Console.Write("Le facteur désiré est le : ");
+                double facto = double.Parse(Console.ReadLine());
+                double resultat = OpérationFactorielle(facto);
+                Console.Write("\nON recommence? ");
+                char ouinon = char.Parse(Console.ReadLine());
+                if (ouinon == 'o')
+                    onrecommence = true;
+                else if (ouinon == 'n')
+                    onrecommence = false;
+            }
+            while (onrecommence);
+
+
+            static double OpérationFactorielle(double n)
+            {
+                double temp = 0;
+                    if (n == 0)
+                        temp = 1;
+                    else
+                        temp = n * OpérationFactorielle(n - 1);
+                Console.WriteLine($"{n} : {temp}");
+                return temp;
+
+            }
+        }
+    }
+}

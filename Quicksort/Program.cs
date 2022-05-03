0260c378dbba29e6a143ec7daf7d804a5fbb97a8
beutchowski <allopalto@icloud.com>
2022-05-03 16:42:13 +0200

    Quicksort début

---
+++ Quicksort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Quicksort
+{
+    internal class Program
+    {
+
+        static int[] tab;
+        static void Main(string[] args)
+        {
+
+            tab = new int[16] {22, 36, 6, 79, 26, 49, 75, 13, 31, 62, 27, 76, 33, 16, 62, 47 };
+
+
+
+
+
+
+        }
+
+        static void Quicksort(int borneMin, int borneMax)
+        {
+            int indexRéf = ((borneMax - borneMin) / 2) + borneMin;
+
+            int valeurRéférence;
+
+
+
+
+
+
+        }
+
+
+
+    }
+}

3103057728338fee2b290737363c9eb6f7c6dfe7
beutchowski <allopalto@icloud.com>
2022-06-07 16:42:33 +0200

    BinSORT ok

--- TempsDeTri/Program.cs
+++ TempsDeTri/Program.cs
@@ -6,7 +6,7 @@ namespace TempsDeTri
     internal class Program
     {
         static Random rand = new Random();
-        const int TAILLE = 10000;
+        const int TAILLE = 10;
         static List<double> stockageSortieBubble;
         static List<double> stockageSortieInser;
         static List<double> stockageSortieSelec;
@@ -16,31 +16,45 @@ namespace TempsDeTri
             stockageSortieBubble = new List<double>();
 
 
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < 2; i++)
             {
                 int[] tab1 = Tab(TAILLE);
                 int[] tab2 = Tab(TAILLE);
                 int[] tab3 = Tab(TAILLE);
+                int[] tab4 = Tab(TAILLE);
                 TriParBubbleSort bubble = new TriParBubbleSort(tab1);
                 TriParInsertion triInser = new TriParInsertion(tab2);
                 TriParSelection triSelec = new TriParSelection(tab3);
+                TriParBinSort triBin = new TriParBinSort(tab4);
 
 
                 bubble.Sort();
 
                 triInser.Sort();
 
-
                 triSelec.Sort();
+
+                foreach(int t in tab4)
+                {
+                    Console.Write($"|{t}");
+                }
+                Console.WriteLine("\n");
+
+                triBin.Sort();
+
+
+                Console.WriteLine("\n");
 
 
                 Console.WriteLine($"Tri BubbleSort test {i} : " + bubble.stopwatch.ElapsedMilliseconds + "ms");
 
                 Console.WriteLine($"Tri par insertion test {i} : " + triInser.stopwatch.ElapsedMilliseconds + "ms");
 
-                //Console.WriteLine($"Tri par insertion test {i} : {triInser.time:F5}ms");
-
                 Console.WriteLine($"Tri par sélection test {i} : " + triSelec.stopwatch.ElapsedMilliseconds + "ms");
+
+                Console.WriteLine($"Tri par Binsort test {i} : " + triBin.stopwatch.ElapsedMilliseconds + "ms");
+
+
 
             }
 
@@ -54,7 +68,7 @@ namespace TempsDeTri
             int[] tab = new int[taille];
             for (int i = 0; i < tab.Length; i++)
             {
-                tab[i] = rand.Next(1,1000000);
+                tab[i] = rand.Next(1,100);
             }
             return tab;
         }
---
+++ TempsDeTri/TriParBinSort.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempsDeTri
+{
+    internal class TriParBinSort
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int[] tab;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Stopwatch stopwatch { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tab"></param>
+        public TriParBinSort(int[] tab)
+        {
+            this.tab = tab;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Sort()
+        {
+            StartChrono();
+            StartTri();
+            StopChrono();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void StartChrono()
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private void StopChrono()
+        {
+            stopwatch.Stop();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void StartTri()
+        {
+            int minValue = tab[0];
+            int maxValue = 0;
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i] > maxValue)
+                    maxValue = tab[i];
+                if (tab[i] < minValue)
+                    minValue = tab[i];
+            }
+
+            int[] tabBin = new int[maxValue + 1];
+
+            for(int i = 0; i < tab.Length; i++)
+            {
+                tabBin[tab[i]] = tab[i];
+            }
+
+            int k = 0;
+            for(int j = 0; j < tabBin.Length; j++)
+            {
+                if(tabBin[j] > 0)
+                {
+                    tab[k] = tabBin[j];
+                    k++;
+                }
+            }
+
+            foreach (int g in tabBin)
+            {
+                Console.Write($"|{g}");
+            }
+
+            Console.WriteLine("\n **************************************************");
+
+            foreach (int t in tab)
+            {
+                Console.Write($"|{t}");
+            }
+
+            
+        }
+
+    }
+}

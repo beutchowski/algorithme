cf4fb63abac2d5efee8aef2b5d067e009f28705b
beutchowski <allopalto@icloud.com>
2022-09-06 16:31:51 +0200

    Partie principale de l'exercice OK

--- DataManagement/Pile.cs
+++ DataManagement/Pile.cs
@@ -8,13 +8,75 @@ namespace DataManagement
 {
     internal class Pile
     {
-        public Pile()
-        {
+        int[] pile;
+        private int sommet;
 
+
+        public Pile(int nbrElément)
+        {
+            this.pile = new int[nbrElément];
+            sommet = 0;
+        }
+
+        public void MettreLeTrucSurLaPile(int valeurAjoutéeSurLaPile)
+        {
+            if (pile.Length > sommet)
+            {
+                pile[sommet] = valeurAjoutéeSurLaPile;
+                sommet++;
+            }
+ 
+            else
+                Console.WriteLine("La pile est pleine!!!!");
+
+        }
+
+        public void RemplacerUnTrucAuSommetdeLaPile(int valeurARemplacerAuSommetDeLaPile)
+        {
+            pile[sommet-1] = valeurARemplacerAuSommetDeLaPile;
+
+        }
+
+        public void DésempilerLaValeurAuSommetDeLaPile()
+        {
+            if (sommet>0)
+            {
+                sommet--;
+            }
+            else
+            {
+                Console.WriteLine("La pile est vide!!!!");
+            }
+        }
+
+
+        public int ValeurAuSommetDeLaPile()
+        {
+            return pile[sommet-1];
+        }
+
+
+        public void PilePleine()
+        {
+            if (sommet == pile.Length)
+                Console.WriteLine("La pile est pleine videz la un peu svp!!!");
+        }
+
+        public void PileVide()
+        {
+            if (sommet == 0)
+                Console.WriteLine("La pile est vide, empiler des truc svp!!!!");
         }
 
 
 
+        public void Display()
+        {
+            for (int i = 0; i < pile.Length; i++)
+            {
+                Console.Write(pile[i] + "|");
+            }
+        }
 
 
     }
--- DataManagement/Program.cs
+++ DataManagement/Program.cs
@@ -16,6 +16,69 @@ namespace DataManagement
             liste1.remove(1);
             Console.WriteLine();
             liste1.Display();
+
+            Console.WriteLine("\n");
+            Console.WriteLine("********Pile**********");
+            Pile pile1 = new Pile(10);
+            pile1.Display();
+            Console.WriteLine("\n");
+            pile1.MettreLeTrucSurLaPile(4);
+            pile1.MettreLeTrucSurLaPile(8);
+            pile1.MettreLeTrucSurLaPile(15);
+            pile1.MettreLeTrucSurLaPile(16);
+            pile1.MettreLeTrucSurLaPile(23);
+            pile1.MettreLeTrucSurLaPile(42);
+            pile1.Display();
+            Console.WriteLine("\n");
+            pile1.RemplacerUnTrucAuSommetdeLaPile(41);
+            pile1.Display();
+            Console.WriteLine("\n");
+            Console.WriteLine("La valeur au sommet de la pile est :" + pile1.ValeurAuSommetDeLaPile());
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.Display();
+            Console.WriteLine("\n");
+            Console.WriteLine("La valeur au sommet de la pile est :" + pile1.ValeurAuSommetDeLaPile());
+            Console.WriteLine("\n");
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.MettreLeTrucSurLaPile(4);
+            pile1.MettreLeTrucSurLaPile(8);
+            pile1.MettreLeTrucSurLaPile(15);
+            pile1.MettreLeTrucSurLaPile(16);
+            pile1.MettreLeTrucSurLaPile(23);
+            pile1.MettreLeTrucSurLaPile(42);
+            pile1.MettreLeTrucSurLaPile(4);
+            pile1.MettreLeTrucSurLaPile(8);
+            pile1.MettreLeTrucSurLaPile(15);
+            pile1.MettreLeTrucSurLaPile(16);
+            Console.WriteLine("\n");
+            pile1.Display();
+            Console.WriteLine("\n");
+            pile1.PilePleine();
+            Console.WriteLine("\n");
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            pile1.DésempilerLaValeurAuSommetDeLaPile();
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            pile1.Display();
+            Console.WriteLine("\n");
+            pile1.PileVide();
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+
+
         }
     }
 }

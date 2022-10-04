1aea7a3ba6fc8b59839934c6d48dfd798b66fe59
beutchowski <allopalto@icloud.com>
2022-10-04 16:43:23 +0200

    A continuer méthodes...

--- DataManagement/Table.cs
+++ DataManagement/Table.cs
@@ -15,7 +15,31 @@ namespace DataManagement
             tab = new int [grandeurTable];
         }
 
+        public int Consulter(int valeurAtrouver)
+        {
+            int index = 0;
+            do
+            {
+                index++; 
+            } while (index < tab.Length && valeurAtrouver != tab[index]);
+            return index;
 
+        }
+
+        public void Insérer(int valeurAInsérer)
+        {
+
+        }
+
+        public int Retirer()
+        {
+            return 0;
+        }
+
+        public void initialiser()
+        {
+
+        }
 
 
 

068fb5f34db3906fb2aa0c94bbe84864c7d3ad6b
beutchowski <allopalto@icloud.com>
2022-10-04 16:28:16 +0200

    Début table

---
+++ DataManagement/Table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManagement
+{
+    internal class Table
+    {
+        int[] tab;
+
+        public Table(int grandeurTable) 
+        {
+            tab = new int [grandeurTable];
+        }
+
+
+
+
+
+
+    }
+
+}
